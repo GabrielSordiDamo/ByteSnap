a7fc1c137916d2b02cf3c7ecbcfd3ca96eaa93f9
GabrielSordi <gabriel.sordi.damo@gmail.com>
2024-11-14 22:55:00 -0300

    bugfix editor disappear on padding

--- src/components/EditorSettingsForm/EdditorSettings.constants.tsx
+++ src/components/EditorSettingsForm/EdditorSettings.constants.tsx
@@ -1,16 +1,13 @@
-export const MAX_EDITOR_WIDTH = window.innerWidth;
-export const MIN_EDITOR_WIDTH = MAX_EDITOR_WIDTH / 10;
-export const DEFAULT_EDITOR_WIDTH = MAX_EDITOR_WIDTH / 2;
+export const MAX_EDITOR_WIDTH = window.innerWidth * 0.9;
+export const MIN_EDITOR_WIDTH = MAX_EDITOR_WIDTH * 0.2;
 
 export const MIN_FONT_SIZE = 0;
 
 export const MAX_FONT_SIZE = 50;
 export const MIN_EDITOR_PADDING = 0;
 
-export const MAX_EDITOR_PADDING = 150;
+export const MAX_EDITOR_PADDING = MIN_EDITOR_WIDTH * 0.2;
 
 export const DEFAULT_EDITOR_PADDING = 10;
 
 export const DEFAULT_EDITOR_BG_COLOR = "#0099FFFF";
-
-export const INITIAl_EDITOR_BG_COLOR = "transparent";
